81feb36f481cb143fa38bdb14e764ec493f8f4d2
pontaGC <makizawawa@gmail.com>
2025-02-09 20:50:09 +0900

    Added dependency registrant.

---
+++ src/PgcSharedLib/DependencyInjection/IDependenyRegistrant.cs
@@ -0,0 +1,15 @@
+﻿namespace PgcSharedLib.DependencyInjection
+{
+    /// <summary>
+    /// The registrant that registers the dependencies to a container.
+    /// </summary>
+    public interface IDependenyRegistrant
+    {
+        /// <summary>
+        /// Registers the services to container.
+        /// </summary>
+        /// <param name="container">The target container to register.</param>
+        /// <exception cref="ArgumentNullException"><c>container</c> is <c>null</c>.</exception>
+        void Register(IDIContainer container);
+    }
+}
