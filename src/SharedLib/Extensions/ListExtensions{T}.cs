4791ea5dd4c24c3dd9b443a64bb864dc712c87fa
pontaGC <makizawawa@gmail.com>
2025-05-05 21:25:42 +0900

    Add ListExtensions.

---
+++ src/SharedLib/Extensions/ListExtensions{T}.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+namespace SharedLib.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="IList{T}"/>.
+    /// </summary>
+    public static class ListExtension
+    {
+        /// <summary>
+        /// Adds an object to the head of the list.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of the source list.</typeparam>
+        /// <param name="source">The list to add.</param>
+        /// <param name="item">
+        /// The object to be added to the end of the <see cref="IList{T}"/>.
+        /// The value can be <c>null</c> for reference types.
+        /// </param>
+        [DebuggerStepThrough]
+        public static void AddToHead<TSource>(this IList<TSource> source, TSource item)
+        {
+            source?.Insert(0, item);
+        }
+
+        /// <summary>
+        /// Removes the last element of the list.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the source list.</typeparam>
+        /// <param name="source">The list to remove.</param>
+        /// <returns><c>true</c> if the last element has been removed, otherwise, <c>false</c>.</returns>
+        [DebuggerStepThrough]
+        public static bool RemoveLast<T>(this IList<T> source)
+        {
+            if (source is null || !source.Any())
+            {
+                return false;
+            }
+
+            if (source.IsReadOnly)
+            {
+                // The read-only list cannot be modified
+                return false;
+            }
+
+            var lastIndex = source.Count - 1;
+            if (lastIndex < 0)
+            {
+                return false;
+            }
+
+            source.RemoveAt(lastIndex);
+            return true;
+        }
+    }
+}
---
+++ test/SharedLib.Test/Extensions/ListExtensionsTest.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.ObjectModel;
+
+using SharedLib.Extensions;
+
+namespace SharedLib.Test.Extensions
+{
+    public class ListExtensionsTest
+    {
+        #region AddToHead
+
+        [Fact]
+        public void AddToHead_DoNothing_WhenNullList()
+        {
+            IList<int> sut = null;
+            int item = 1;
+
+            sut.AddToHead(item);
+
+            Assert.Null(sut);
+        }
+
+        [Fact]
+        public void AddToHead_AddItemToEmptyList()
+        {
+            IList<int> sut = new List<int>();
+            int item = 1;
+
+            sut.AddToHead(item);
+
+            Assert.Single(sut, item);
+        }
+
+        [Fact]
+        public void AddToHead_AddItemToList()
+        {
+            IList<int> sut = new List<int>() { 2, 3 };
+            int item = 1;
+
+            sut.AddToHead(item);
+
+            Assert.Equal(3, sut.Count);
+            Assert.Equal(item, sut.First());
+        }
+
+        #endregion
+
+        #region RemoveLast
+
+        [Fact]
+        public void RemoveLast_EmptyList()
+        {
+            var sut = new List<int>();
+            var actual = sut.RemoveLast();
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void RemoveLast_ReadOnlyList()
+        {
+            var list = new List<string>() { "One", "Two", "Three" };
+            var sut = new ReadOnlyCollection<string>(list);
+
+            var actual = sut.RemoveLast();
+
+            Assert.False(actual);
+            Assert.Collection(sut,
+                item => Assert.Equal("One", item),
+                item => Assert.Equal("Two", item),
+                item => Assert.Equal("Three", item));
+        }
+
+        [Fact]
+        public void RemoveLast_Success()
+        {
+            var sut = new List<string>() { "One", "Two", "Three" };
+
+            var actual = sut.RemoveLast();
+
+            Assert.True(actual);
+            Assert.DoesNotContain("Three", sut);
+            Assert.Equal(2, sut.Count);
+        }
+
+        #endregion
+    }
+}
