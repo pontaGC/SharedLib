1b40b1577066b85e37537fe17506b4caf190adc3
pontaGC <makizawawa@gmail.com>
2025-06-07 21:22:42 +0900

    Add Get RGB/ARGB code.

---
+++ src/SharedLib/Extensions/ColorExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+namespace SharedLib.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="Color" />
+    /// </summary>
+    public static class ColorExtensions
+    {
+        /// <summary>
+        /// Gets a RGB color code for the given color.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The RGB color code (e.g. "#1E90FF").</returns>
+        [DebuggerStepThrough]
+        public static string GetRGBCode(this Color color)
+        {
+            return $"#{color.R:X2}{color.G:X2}{color.B:X2}";
+        }
+
+        /// <summary>
+        /// Gets a ARGB color code with alpha value for the given color.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The ARGB code (e.g. "#FF1E90FF").</returns>
+        [DebuggerStepThrough]
+        public static string GetARGBCode(this Color color)
+        {
+            return $"#{color.A:X2}{color.R:X2}{color.G:X2}{color.B:X2}";
+        }
+    }
+}
---
+++ test/SharedLib.Test/Extensions/ColorExtensionsTests.cs
@@ -0,0 +1,108 @@
+﻿using System.Drawing;
+
+using FluentAssertions;
+using SharedLib.Extensions;
+
+namespace SharedLib.Test.Extensions
+{
+    public class ColorExtensionsTests
+    {
+        #region GetRGBCode
+
+        [Fact]
+        public void GetRGBCode_Aquamarine()
+        {
+            var sut = Color.FromArgb(127, 255, 212);
+
+            var actual = sut.GetRGBCode();
+
+            actual.Should().Be("#7FFFD4");
+        }
+
+        [Fact]
+        public void GetRGBCode_Black()
+        {
+            var sut = Color.FromArgb(0, 0, 0);
+
+            var actual = sut.GetRGBCode();
+
+            actual.Should().Be("#000000");
+        }
+
+        [Fact]
+        public void GetRGBCode_White()
+        {
+            var sut = Color.FromArgb(255, 255, 255);
+
+            var actual = sut.GetRGBCode();
+
+            actual.Should().Be("#FFFFFF");
+        }
+
+        [Fact]
+        public void GetRGBCode_Red()
+        {
+            var sut = Color.FromArgb(255, 0, 0);
+
+            var actual = sut.GetRGBCode();
+
+            actual.Should().Be("#FF0000");
+        }
+
+        [Fact]
+        public void GetRGBCode_Empty()
+        {
+            var sut = Color.Empty;
+
+            var actual = sut.GetRGBCode();
+
+            actual.Should().Be("#000000");
+        }
+
+        #endregion
+
+        #region GetARGBCode
+
+        [Fact]
+        public void GetARGBCode_TranslucentAquamarine()
+        {
+            var sut = Color.FromArgb(128, 127, 255, 212);
+
+            var actual = sut.GetARGBCode();
+
+            actual.Should().Be("#807FFFD4");
+        }
+
+        [Fact]
+        public void GetARGBCode_OpaqueBlack()
+        {
+            var sut = Color.FromArgb(255, 0, 0, 0);
+
+            var actual = sut.GetARGBCode();
+
+            actual.Should().Be("#FF000000");
+        }
+
+        [Fact]
+        public void GetARGBCode_WhiteWithDefaultAlpha()
+        {
+            var sut = Color.FromArgb(255, 255, 255);
+
+            var actual = sut.GetARGBCode();
+
+            actual.Should().Be("#FFFFFFFF");
+        }
+
+        [Fact]
+        public void GetARGBCode_Empty()
+        {
+            var sut = Color.Empty;
+
+            var actual = sut.GetARGBCode();
+
+            actual.Should().Be("#00000000");
+        }
+
+        #endregion
+    }
+}
