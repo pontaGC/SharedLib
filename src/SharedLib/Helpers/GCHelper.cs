a111debe24c64ea8114a32ccf11b8388bd5a2f51
pontaGC <makizawawa@gmail.com>
2025-06-28 21:21:45 +0900

    Created GCHelper.

---
+++ src/SharedLib/Helpers/GCHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace SharedLib.Helpers
+{
+    /// <summary>
+    /// Helper for garbage collection.
+    /// </summary>
+    public static class GCHelper
+    {
+        /// <summary>
+        /// Collects all garbage and waits for finalizers to complete.
+        /// </summary>
+        public static void FullCollect()
+        {
+            GC.Collect(GC.MaxGeneration);
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
+}
